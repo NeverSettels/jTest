153fab79083ba76c6c5e77065ae518af6f72f652
Sean Downs <spdowns@protonmail.com>
2020-03-23 15:03:02 -0700

    add callback to scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,11 @@
-jQuery('img').click(function(){
-  alert("this is an image");
-});
-jQuery("h1").click(function(){
-  alert('this is a header');
-});
-jQuery('p').click(function(){
-  alert("this is a paragraph");
+jQuery(document).ready(function() {
+  jQuery('img').click(function(){
+    alert("this is an image");
+  });
+  jQuery("h1").click(function(){
+    alert('this is a header');
+  });
+  jQuery('p').click(function(){
+    alert("this is a paragraph");
+  });
 });
