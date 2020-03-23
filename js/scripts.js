d25a9fffa6a4545f2027c1acf63083511d86db35
Sean Downs <spdowns@protonmail.com>
2020-03-23 14:53:35 -0700

    add function to scrips.js

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+jQuery('img').click(function(){
+  alert("this is an image");
+});
+jQuery("h1").click(function(){
+  alert('this is a header');
+});
+jQuery('p').click(function(){
+  alert("this is a paragraph");
+});
